139c2f14d9817091a86630be04503e4161379afa
arda-onur <ardaonur15@gmail.com>
2025-07-31 14:50:46 +0300

    User Request class added

---
+++ evrensesi/src/main/java/com/evrensesi/evrensesi/request/UserRequest.java
@@ -0,0 +1,22 @@
+package com.evrensesi.evrensesi.request;
+
+import com.evrensesi.evrensesi.utility.regex.EmailRegexPattern;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserRequest(
+        @NotBlank(message = "{create.user.request.username.not.blank.message}")
+        @Size(message = "{create.user.request.username.size.message}", min = 4, max = 255)
+        String username,
+        @NotBlank(message = "{create.user.request.password.not.blank.message}")
+        @Size(message = "{create.user.request.password.size.message}", min = 4, max = 255)
+        String password,
+        @NotBlank(message = "{create.user.request.confirmPassword.not.blank.message}")
+        @Size(message = "{create.user.request.confirmPassword.size.message}", min = 4, max = 255)
+        String confirmPassword,
+        @NotBlank(message = "{create.user.request.email.not.blank.message}")
+        @Email(regexp = EmailRegexPattern.EMAIL_PATTERN)
+        String email) {
+
+}

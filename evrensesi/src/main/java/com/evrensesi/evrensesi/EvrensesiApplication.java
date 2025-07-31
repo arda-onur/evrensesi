467d0d9a6a270cff6568fc8a47ad88f8ceeeaaef
arda-onur <ardaonur15@gmail.com>
2025-07-31 14:52:23 +0300

    ..

---
+++ evrensesi/src/main/java/com/evrensesi/evrensesi/EvrensesiApplication.java
@@ -0,0 +1,13 @@
+package com.evrensesi.evrensesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvrensesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvrensesiApplication.class, args);
+	}
+
+}
---
+++ evrensesi/src/test/java/com/evrensesi/evrensesi/EvrensesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evrensesi.evrensesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvrensesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

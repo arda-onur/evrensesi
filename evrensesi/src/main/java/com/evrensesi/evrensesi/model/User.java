5ecd854d46e428d4ab8906941e758ac13608a163
arda-onur <ardaonur15@gmail.com>
2025-08-18 12:23:44 +0300

    Added enabled accountnonlocked , nonexpired and credentialsNonexpired and methods for UserEntity

--- evrensesi/src/main/java/com/evrensesi/evrensesi/model/User.java
+++ evrensesi/src/main/java/com/evrensesi/evrensesi/model/User.java
@@ -2,6 +2,7 @@ package com.evrensesi.evrensesi.model;
 
 import com.evrensesi.evrensesi.constant.Role;
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -12,25 +13,46 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 @Entity
-@NoArgsConstructor
-@RequiredArgsConstructor
+@Table(name = "users")
 @Setter
 public class User implements UserDetails {
+
 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable=false, unique=true)
     private String username;
+
+    @Column(nullable=false)
     private String password;
+    @Getter
+    @Column(nullable=false, unique=true)
     private String email;
-    private boolean isAccountNonLocked;
+
     @Enumerated(EnumType.STRING)
-    private Role role;
-    @OneToOne(mappedBy = "user",cascade = CascadeType.ALL, orphanRemoval = true)
+    @Column(nullable=false)
+    private Role role = Role.USER;
+
+    @Column(nullable=false)
+    private boolean enabled = true;
+
+    @Column(nullable=false)
+    private boolean accountNonLocked = true;
+
+    @Column(nullable=false)
+    private boolean accountNonExpired = true;
+
+    @Column(nullable=false)
+    private boolean credentialsNonExpired = true;
+
+    @OneToOne
+    @JoinColumn(name = "comment_id", unique = true)
     private Comment comment;
 
-
+    // ---- UserDetails ----
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
@@ -46,14 +68,10 @@ public class User implements UserDetails {
         return this.username;
     }
 
-   public String getEmail() {
-        return this.email;
-   }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return this.isAccountNonLocked;
-    }
-
-
+    @Override public boolean isEnabled() { return enabled; }
+    @Override public boolean isAccountNonLocked() { return accountNonLocked; }
+    @Override public boolean isAccountNonExpired() { return accountNonExpired; }
+    @Override public boolean isCredentialsNonExpired() { return credentialsNonExpired; }
 }
+
+

297d3ae81be44c8facf5c264ca5958fb6630d70e
arda-onur <ardaonur15@gmail.com>
2025-08-18 12:21:32 +0300

    Adding @JsonIgnore for UserDto

--- evrensesi/src/main/java/com/evrensesi/evrensesi/dto/UserDto.java
+++ evrensesi/src/main/java/com/evrensesi/evrensesi/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.evrensesi.evrensesi.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,9 @@ import lombok.Setter;
 @Setter
 public class UserDto {
     private String username;
+    @JsonIgnore
     private String password;
+    @JsonIgnore
     private String confirmPassword;
     private String email;
 }

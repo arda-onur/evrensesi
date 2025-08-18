35aa1dbd29be84848d2553c4fa89565d7276a03b
arda-onur <ardaonur15@gmail.com>
2025-08-18 12:25:27 +0300

    Changed BcryptPassword encoder to PasswordEncoderFactories

--- evrensesi/src/main/java/com/evrensesi/evrensesi/configuration/PasswordConfiguration.java
+++ evrensesi/src/main/java/com/evrensesi/evrensesi/configuration/PasswordConfiguration.java
@@ -3,6 +3,7 @@ package com.evrensesi.evrensesi.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -10,6 +11,6 @@ public class PasswordConfiguration {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }

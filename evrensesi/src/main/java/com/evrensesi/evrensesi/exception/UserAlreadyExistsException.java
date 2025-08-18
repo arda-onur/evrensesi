5a0f7561518f405e48afe966f12612bb272dec22
arda-onur <ardaonur15@gmail.com>
2025-08-18 12:23:09 +0300

    Changed responseStatus massages

--- evrensesi/src/main/java/com/evrensesi/evrensesi/exception/UserAlreadyExistsException.java
+++ evrensesi/src/main/java/com/evrensesi/evrensesi/exception/UserAlreadyExistsException.java
@@ -3,7 +3,7 @@ package com.evrensesi.evrensesi.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "user.already.exists.message")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Username or Email already exist!")
 public class UserAlreadyExistsException extends RuntimeException {
     private final String username;
     public UserAlreadyExistsException(String message,String username) {

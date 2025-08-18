7f4a34e007be66875f8a329939e1842b47e74405
arda-onur <ardaonur15@gmail.com>
2025-08-18 12:21:10 +0300

    Implemented Mapping annotation for User

--- evrensesi/src/main/java/com/evrensesi/evrensesi/mapper/UserMapper.java
+++ evrensesi/src/main/java/com/evrensesi/evrensesi/mapper/UserMapper.java
@@ -4,11 +4,18 @@ import com.evrensesi.evrensesi.dto.UserDto;
 import com.evrensesi.evrensesi.model.User;
 import com.evrensesi.evrensesi.request.UserRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Mapper(config = MapperConfiguration.class)
+@Component
 public interface UserMapper {
-
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "comment", ignore = true)
+    @Mapping(target = "authorities", ignore = true)
+    @Mapping(target = "accountNonLocked", ignore = true)
     User map(UserRequest userRequest);
-
     UserDto map(User user);
 }

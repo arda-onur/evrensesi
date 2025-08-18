564fb2f6ab2dc0cb4fb0d3ceae3ac171f5b0d3fa
arda-onur <ardaonur15@gmail.com>
2025-08-18 12:25:40 +0300

    Implemented JWT related methods

---
+++ evrensesi/src/main/java/com/evrensesi/evrensesi/service/JwtService.java
@@ -0,0 +1,21 @@
+package com.evrensesi.evrensesi.service;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+public interface JwtService {
+    String extractUsername(String token);
+    Claims extractAllClaims(String token);
+    Key getSignInKey();
+    <T> T extractClaim(String token, Function<Claims,T> claimsResolver);
+    String generateToken(Map<String, Object> extraClaims, UserDetails userDetails);
+    public String generateToken(UserDetails userDetails);
+    boolean isTokenValid(String token, UserDetails userDetails);
+    boolean isTokenExpired(String token);
+    Date extractExpiration(String token);
+}
---
+++ evrensesi/src/main/java/com/evrensesi/evrensesi/service/implementation/JwtServiceImpl.java
@@ -0,0 +1,86 @@
+package com.evrensesi.evrensesi.service.implementation;
+
+import com.evrensesi.evrensesi.service.JwtService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+
+
+    private static final String SECRET_KEY = "AaPrZ1fRfMVxWK/0KZaq6RheyHjoj5kO5yX6c2G5SM1O8QcTf3t6FjR6UD3VOQY6";
+
+
+    @Override
+    public String extractUsername(String jwtToken) {
+
+       return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    @Override
+     public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    @Override
+    public Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    @Override
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    @Override
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    @Override
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    @Override
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    @Override
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    @Override
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+}

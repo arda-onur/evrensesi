2b554c118f978c0985496df6e0137dd4a841a2ec
arda-onur <ardaonur15@gmail.com>
2025-09-13 11:59:11 +0300

    Added CommentDto.java

---
+++ evrensesi/src/main/java/com/evrensesi/evrensesi/dto/CommentDto.java
@@ -0,0 +1,13 @@
+package com.evrensesi.evrensesi.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommentDto {
+    private String comment;
+    private double x;
+    private double y;
+    private String author;
+}

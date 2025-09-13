5cd22e12fb9e2df1d2102533cbc97ce4ad479f53
arda-onur <ardaonur15@gmail.com>
2025-09-13 12:01:29 +0300

    Changed SQL table code

--- evrensesi/src/main/resources/db/sql/V_1_0_1__createcommenttable.sql
+++ evrensesi/src/main/resources/db/sql/V_1_0_1__createcommenttable.sql
@@ -1,4 +1,11 @@
-CREATE TABLE IF NOT EXISTS comment (
-                                       id BIGSERIAL PRIMARY KEY,
-                                       comment TEXT
+create table comment (
+                         id bigserial primary key,
+                         comment varchar(255),
+                         x double precision not null,
+                         y double precision not null,
+                         username varchar(255) not null unique,
+
+                         constraint fk_comment_user
+                             foreign key (username)
+                                 references users (username)         
 );

0810b9976b896afc433d079a5f93d7de8ccf759c
Arda Onur <ardaonur15@gmail.com>
2026-03-02 14:35:20 +0300

    Initial commit

---
+++ src/test/java/com/arda/evrensesi/EvrensesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arda.evrensesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvrensesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

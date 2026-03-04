f6a6cea69bc32b8bd9a21cddb7f32602a9ec54ff
Arda Onur <ardaonur15@gmail.com>
2026-03-04 12:54:00 +0300

    Implemented UserMapper

---
+++ src/main/java/com/arda/evrensesi/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package com.arda.evrensesi.mapper;
+
+import com.arda.evrensesi.dto.UserDTO;
+import com.arda.evrensesi.entity.User;
+import com.arda.evrensesi.request.UserRequest;
+
+public class UserMapper {
+    private UserMapper() {}
+
+    public static User toEntity(UserRequest req) {
+        if (req == null) return null;
+
+        User user = new User();
+        user.setEmail(req.email());
+
+        return user;
+    }
+
+    public static UserDTO toDTO(User user) {
+        if (user == null) return null;
+
+        return new UserDTO(
+                user.getEmail(),
+                user.getCreatedAt()
+        );
+    }
+}

e4cabe5a8f812e229aa194319d96218d5e14179d
Arda Onur <ardaonur15@gmail.com>
2026-03-05 13:58:16 +0300

    Implemented findByEmail query method

--- src/main/java/com/arda/evrensesi/repository/UserRepository.java
+++ src/main/java/com/arda/evrensesi/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -17,7 +18,10 @@ public interface UserRepository extends JpaRepository<User,UUID> {
                 WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE
               END
             FROM User u
-            WHERE u.email = :email
-            """)
+            WHERE u.email = :email""")
     boolean existsByEmail(@Param("email") String email);
+
+    @Query("""
+           SELECT u FROM User u WHERE u.email = :email""")
+    Optional<User> findByEmail(@Param("email") String email);
 }

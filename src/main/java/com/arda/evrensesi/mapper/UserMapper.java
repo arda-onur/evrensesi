46c87bbe2e95b587b1aa8c9b5cc95b1185ecdc71
Arda Onur <ardaonur15@gmail.com>
2026-03-06 12:01:08 +0300

    Changed UserRequest to RegisterRequest

--- src/main/java/com/arda/evrensesi/mapper/UserMapper.java
+++ src/main/java/com/arda/evrensesi/mapper/UserMapper.java
@@ -2,12 +2,12 @@ package com.arda.evrensesi.mapper;
 
 import com.arda.evrensesi.dto.UserDTO;
 import com.arda.evrensesi.entity.User;
-import com.arda.evrensesi.request.UserRequest;
+import com.arda.evrensesi.request.RegisterRequest;
 
 public class UserMapper {
     private UserMapper() {}
 
-    public static User toEntity(UserRequest req) {
+    public static User toEntity(RegisterRequest req) {
         if (req == null) return null;
 
         User user = new User();

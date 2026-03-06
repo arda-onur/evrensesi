7e7ebf9c73e6be9d0d7a0e611568a8b5dfde0447
Arda Onur <ardaonur15@gmail.com>
2026-03-06 12:02:43 +0300

    Added login method

--- src/main/java/com/arda/evrensesi/service/UserService.java
+++ src/main/java/com/arda/evrensesi/service/UserService.java
@@ -1,9 +1,13 @@
 package com.arda.evrensesi.service;
 
 import com.arda.evrensesi.dto.UserDTO;
-import com.arda.evrensesi.request.UserRequest;
+import com.arda.evrensesi.request.LoginRequest;
+import com.arda.evrensesi.request.RegisterRequest;
+import jakarta.servlet.http.HttpServletRequest;
+
 
 
 public interface UserService {
-    UserDTO register(UserRequest userRequest);
+    UserDTO register(RegisterRequest registerRequest);
+    void login(LoginRequest registerRequest, HttpServletRequest httpRequest);
 }

447b55c0e8bbee6d19edb72e8d4adca3786b88f3
Arda Onur <ardaonur15@gmail.com>
2026-03-08 11:52:09 +0300

    Implemented StarAlreadyExists Exception

---
+++ src/main/java/com/arda/evrensesi/exception/StarAlreadyExistsException.java
@@ -0,0 +1,12 @@
+package com.arda.evrensesi.exception;
+
+import lombok.Getter;
+
+@Getter
+public class StarAlreadyExistsException extends RuntimeException {
+    private final Object[] args;
+    public StarAlreadyExistsException(String message,Object... args) {
+        super(message);
+        this.args = args;
+    }
+}

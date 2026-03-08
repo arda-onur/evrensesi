ae5036dd60c4bd3176963ced56c41e45f175e767
Arda Onur <ardaonur15@gmail.com>
2026-03-08 17:57:25 +0300

    Implemented CustomUserDetailsService

---
+++ src/main/java/com/arda/evrensesi/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.arda.evrensesi.security;
+
+import com.arda.evrensesi.entity.User;
+import com.arda.evrensesi.repository.UserRepository;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @NotNull
+    @Override
+    public UserDetails loadUserByUsername(@NotNull String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("ROLE_USER")
+                .build();
+    }
+}

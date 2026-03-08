113e4dafab00814963e9c785cb3affc7400eec6b
Arda Onur <ardaonur15@gmail.com>
2026-03-08 11:52:33 +0300

    Implemented StarRepository

---
+++ src/main/java/com/arda/evrensesi/repository/StarRepository.java
@@ -0,0 +1,24 @@
+package com.arda.evrensesi.repository;
+
+import com.arda.evrensesi.dto.StarPointDTO;
+import com.arda.evrensesi.entity.Star;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface StarRepository extends JpaRepository<Star, UUID> {
+
+    @Query("""
+            SELECT new StarPointResponse(s.x, s.y)
+            FROM Star s
+            """)
+    List<StarPointDTO> findAllPoints();
+
+
+    boolean existsByUserEmail(String email);
+}

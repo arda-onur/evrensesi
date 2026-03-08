c05ac6a3bf7dc6417fae9644ac76a77e8ab73f6e
Arda Onur <ardaonur15@gmail.com>
2026-03-08 17:57:47 +0300

    Implemented createStar method

---
+++ src/main/java/com/arda/evrensesi/controller/StarController.java
@@ -0,0 +1,29 @@
+package com.arda.evrensesi.controller;
+
+import com.arda.evrensesi.request.StarRequest;
+import com.arda.evrensesi.service.StarService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/star")
+public class StarController {
+
+    private final StarService starService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Void> createStar(@Valid @RequestBody StarRequest starRequest) {
+        starService.createStar(starRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+}

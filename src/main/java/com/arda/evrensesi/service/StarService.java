0531129abaab95359e51ab641f2c7b42a7118300
Arda Onur <ardaonur15@gmail.com>
2026-03-09 13:16:49 +0300

    Added getAllStarCoordinates method

--- src/main/java/com/arda/evrensesi/service/StarService.java
+++ src/main/java/com/arda/evrensesi/service/StarService.java
@@ -1,9 +1,12 @@
 package com.arda.evrensesi.service;
 
-import com.arda.evrensesi.dto.StarPointDTO;
+import com.arda.evrensesi.dto.StarCoordinatesDTO;
 import com.arda.evrensesi.request.StarRequest;
+import org.springframework.data.domain.Page;
+
 
 
 public interface StarService {
-    StarPointDTO createStar(StarRequest starRequest);
+    StarCoordinatesDTO createStar(StarRequest starRequest);
+    Page<StarCoordinatesDTO> getAllStarCoordinates(int page, int size);
 }

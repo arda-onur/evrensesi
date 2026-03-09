db7049349ca0c1e75e88a356cc00652682f4df81
Arda Onur <ardaonur15@gmail.com>
2026-03-09 13:09:23 +0300

    Implemented handler for IllegalArgument and MethodNotValid Exception

--- src/main/java/com/arda/evrensesi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/arda/evrensesi/exception/GlobalExceptionHandler.java
@@ -1,12 +1,16 @@
 package com.arda.evrensesi.exception;
 
 
+import com.arda.evrensesi.exception.customException.StarAlreadyExistsException;
+import com.arda.evrensesi.exception.customException.StarCreationException;
+import com.arda.evrensesi.exception.customException.UserRegistrationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -31,9 +35,9 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(msg);
     }
 
-    @ExceptionHandler(StarAlreadyExistsException.class)
+    @ExceptionHandler({StarAlreadyExistsException.class, StarCreationException.class})
     public ResponseEntity<String> handleStarAlreadyExistsException(StarAlreadyExistsException ex, Locale locale) {
-        log.warn("Star Already Exists: key={}, args={}", ex.getMessage(), ex.getArgs());
+        log.warn("Star conflict occurred: messageKey={}", ex.getMessage());
 
         String msg = messageSource.getMessage(ex.getMessage(), null , ex.getMessage(), locale);
 
@@ -45,5 +49,26 @@ public class GlobalExceptionHandler {
         log.warn("Login failed: {}", ex.getClass().getSimpleName());
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex, Locale locale) {
+
+        log.warn("Illegal argument: key={}", ex.getMessage());
+
+        String msg = messageSource.getMessage(ex.getMessage(), null, ex.getMessage(), locale);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(msg);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodNotValidArgumentException(MethodArgumentNotValidException ex,
+                                                                                                   Locale locale) {
+        String key = ex.getBindingResult().getFieldError().getDefaultMessage();
+
+        String msg = messageSource.getMessage(key, null, key, locale);
+
+        log.warn("Validation error: {}", key);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(msg);
     }
 }

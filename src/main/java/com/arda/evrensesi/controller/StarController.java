6a759032a54398601d0f2fd3720a06a0685e187c
Arda Onur <ardaonur15@gmail.com>
2026-03-09 13:17:13 +0300

    Added getAllStarCoordinates method and its endpoint

--- src/main/java/com/arda/evrensesi/controller/StarController.java
+++ src/main/java/com/arda/evrensesi/controller/StarController.java
@@ -1,16 +1,14 @@
 package com.arda.evrensesi.controller;
 
+import com.arda.evrensesi.dto.StarCoordinatesDTO;
 import com.arda.evrensesi.request.StarRequest;
 import com.arda.evrensesi.service.StarService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -24,6 +22,14 @@ public class StarController {
     public ResponseEntity<Void> createStar(@Valid @RequestBody StarRequest starRequest) {
         starService.createStar(starRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/points")
+    public ResponseEntity<Page<StarCoordinatesDTO>> getAllStarCoordinates(
+            @RequestParam int page,
+            @RequestParam int size) {
+
+        return ResponseEntity.ok(this.starService.getAllStarCoordinates(page, size));
     }
 
 }

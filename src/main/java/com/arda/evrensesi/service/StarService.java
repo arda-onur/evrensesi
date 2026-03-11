e7e2ac8a9f1fdc06040ce11c457f80430ffb718e
Arda Onur <ardaonur15@gmail.com>
2026-03-11 11:56:22 +0300

    Deleted empty lines

--- src/main/java/com/arda/evrensesi/service/StarService.java
+++ src/main/java/com/arda/evrensesi/service/StarService.java
@@ -4,8 +4,6 @@ import com.arda.evrensesi.dto.StarCoordinatesDTO;
 import com.arda.evrensesi.request.StarRequest;
 import org.springframework.data.domain.Page;
 
-
-
 public interface StarService {
     StarCoordinatesDTO createStar(StarRequest starRequest);
     Page<StarCoordinatesDTO> getAllStarCoordinates(int page, int size);

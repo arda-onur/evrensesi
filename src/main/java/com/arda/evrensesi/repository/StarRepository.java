4ffc558957d251acbfb2a877a8598b415f74cf58
Arda Onur <ardaonur15@gmail.com>
2026-03-16 12:30:47 +0300

    Changed import directory to  com.arda.evrensesi.model.entity.Star;

--- src/main/java/com/arda/evrensesi/repository/StarRepository.java
+++ src/main/java/com/arda/evrensesi/repository/StarRepository.java
@@ -1,14 +1,13 @@
 package com.arda.evrensesi.repository;
 
 import com.arda.evrensesi.dto.StarCoordinatesDTO;
-import com.arda.evrensesi.entity.Star;
+import com.arda.evrensesi.model.entity.Star;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository

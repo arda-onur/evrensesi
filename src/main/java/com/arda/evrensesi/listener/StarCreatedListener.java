89f83170f822ff3921c71967079bb895cde88f0f
Arda Onur <ardaonur15@gmail.com>
2026-03-17 11:15:07 +0300

    Implemented StarCreatedListener

--- src/main/java/com/arda/evrensesi/listener/StarCreatedListener.java
+++ src/main/java/com/arda/evrensesi/listener/StarCreatedListener.java
@@ -4,22 +4,39 @@ import com.arda.evrensesi.event.StarCreatedEvent;
 import com.arda.evrensesi.mapper.search.StarDocumentMapper;
 import com.arda.evrensesi.model.document.StarDocument;
 import com.arda.evrensesi.repository.StarESRepository;
+import com.arda.evrensesi.service.StarIndexStatusService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
+@Slf4j
 public class StarCreatedListener {
     private final StarESRepository starESRepository;
+    private final StarIndexStatusService starIndexStatusService;
 
-    public StarCreatedListener(StarESRepository starESRepository) {
+    public StarCreatedListener(StarESRepository starESRepository, StarIndexStatusService starIndexStatusService) {
         this.starESRepository = starESRepository;
+        this.starIndexStatusService = starIndexStatusService;
     }
 
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handleStarCreatedEvent(StarCreatedEvent starCreatedEvent){
-        StarDocument starDocument = StarDocumentMapper.toDocument(starCreatedEvent);
-        starESRepository.save(starDocument);
+    public void handleStarCreatedEvent(StarCreatedEvent starCreatedEvent) {
+        try {
+            log.info("Starting Elasticsearch indexing for starId={}", starCreatedEvent.id());
 
+            StarDocument starDocument = StarDocumentMapper.toDocument(starCreatedEvent);
+            starESRepository.save(starDocument);
+
+            starIndexStatusService.markIndexed(starCreatedEvent.id());
+
+            log.info("Star successfully indexed into Elasticsearch. starId={}", starCreatedEvent.id());
+        } catch (Exception ex) {
+            log.error("Elasticsearch indexing failed for starId={}", starCreatedEvent.id(), ex);
+        }
     }
+
 }

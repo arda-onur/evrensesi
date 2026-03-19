9d15d449a4e22b25bb547dd34544b1add6f48c78
Arda Onur <ardaonur15@gmail.com>
2026-03-19 21:46:32 +0300

    Added findUserStar method

--- src/main/java/com/arda/evrensesi/repository/StarRepository.java
+++ src/main/java/com/arda/evrensesi/repository/StarRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -28,4 +30,12 @@ public interface StarRepository extends JpaRepository<Star, UUID> {
            WHERE s.esIndexed = false
            """)
     List<Star> findAllByEsIndexedFalse();
+
+    @Query("""
+    SELECT new com.arda.evrensesi.dto.StarCoordinatesDTO(s.x, s.y)
+    FROM Star s
+    WHERE s.user.email = :email
+""")
+    Optional<StarCoordinatesDTO> findUserStar(@Param("email") String email);
+
 }

abe86212b4ffc95cec22edd8a3fe177c48521f53
Arda Onur <ardaonur15@gmail.com>
2026-03-19 21:45:51 +0300

    Added servlet response parametre to login method

--- src/main/java/com/arda/evrensesi/controller/AuthController.java
+++ src/main/java/com/arda/evrensesi/controller/AuthController.java
@@ -6,15 +6,14 @@ import com.arda.evrensesi.request.LoginRequest;
 import com.arda.evrensesi.request.RegisterRequest;
 import com.arda.evrensesi.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,9 +31,10 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletRequest httpRequest){
+    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletRequest httpRequest,
+                                      HttpServletResponse httpResponse){
         log.info("Login request received email={}", loginRequest.email());
-        this.userService.login(loginRequest,httpRequest);
+        this.userService.login(loginRequest,httpRequest,httpResponse);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }

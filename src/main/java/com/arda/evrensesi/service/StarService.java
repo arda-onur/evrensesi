02465ad4e0ecb2f13a62b3e1ec698495a0831597
Arda Onur <ardaonur15@gmail.com>
2026-03-19 21:47:40 +0300

    added search and getUserStar method

--- src/main/java/com/arda/evrensesi/service/StarService.java
+++ src/main/java/com/arda/evrensesi/service/StarService.java
@@ -1,10 +1,15 @@
 package com.arda.evrensesi.service;
 
 import com.arda.evrensesi.dto.StarCoordinatesDTO;
+import com.arda.evrensesi.model.document.StarDocument;
 import com.arda.evrensesi.request.StarRequest;
 import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public interface StarService {
     StarCoordinatesDTO createStar(StarRequest starRequest);
     Page<StarCoordinatesDTO> getAllStarCoordinates(int page, int size);
+    StarCoordinatesDTO getUserStar();
+    List<StarCoordinatesDTO> search(String keyword);
 }

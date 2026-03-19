4426a4f1eb9849b3dfaf44891387170d2188b33a
Arda Onur <ardaonur15@gmail.com>
2026-03-19 21:47:10 +0300

    added blank spaces

--- src/main/java/com/arda/evrensesi/request/StarRequest.java
+++ src/main/java/com/arda/evrensesi/request/StarRequest.java
@@ -1,28 +1,28 @@
-package com.arda.evrensesi.request;
+        package com.arda.evrensesi.request;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+        import jakarta.validation.constraints.Max;
+        import jakarta.validation.constraints.Min;
+        import jakarta.validation.constraints.NotBlank;
+        import jakarta.validation.constraints.Size;
 
-public record StarRequest(
-        @NotBlank(message = "{star.message.cannot.be.blank}")
-        @Size(max = 600, message = "{star.message.max.characters}")
-        String message,
+        public record StarRequest(
+                @NotBlank(message = "{star.message.cannot.be.blank}")
+                @Size(max = 600, message = "{star.message.max.characters}")
+                String message,
 
-        @Min(value = 0, message = "{star.coordinate.x.min}")
-        @Max(value = 10000, message = "{star.coordinate.x.max}")
-        int x,
+                @Min(value = 0, message = "{star.coordinate.x.min}")
+                @Max(value = 10000, message = "{star.coordinate.x.max}")
+                int x,
 
-        @Min(value = 0, message = "{star.coordinate.y.min}")
-        @Max(value = 10000, message = "{star.coordinate.y.max}")
-        int y
-) {
-    public StarRequest {
-        int wordCount = message.trim().split("\\s+").length;
+                @Min(value = 0, message = "{star.coordinate.y.min}")
+                @Max(value = 10000, message = "{star.coordinate.y.max}")
+                int y
+        ) {
+            public StarRequest {
+                int wordCount = message.trim().split("\\s+").length;
 
-        if (wordCount > 50) {
-            throw new IllegalArgumentException("star.message.max.words");
+                if (wordCount > 50) {
+                    throw new IllegalArgumentException("star.message.max.words");
+                }
+            }
         }
-    }
-}

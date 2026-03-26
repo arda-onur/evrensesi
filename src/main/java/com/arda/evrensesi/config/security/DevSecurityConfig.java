2545c7add046b638b6e7f8f5b98796014e4b4170
Arda Onur <ardaonur15@gmail.com>
2026-03-26 14:01:42 +0300

    Added dev sec file

---
+++ src/main/java/com/arda/evrensesi/config/security/DevSecurityConfig.java
@@ -0,0 +1,79 @@
+package com.arda.evrensesi.config.security;
+
+import com.arda.evrensesi.security.CustomUserDetailsService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@Profile("dev")
+public class DevSecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public DevSecurityConfig(CustomUserDetailsService customUserDetailsService,
+                             PasswordEncoder passwordEncoder) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers( "/h2-console/**","/auth/login", "/auth/register","/star/points",
+                                                "/star/getMessage","/actuator/health", "/actuator/prometheus","/actuator/metrics").permitAll()
+                        .anyRequest().authenticated()
+                )
+                    .sessionManagement(session ->
+                            session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .logout(logout ->
+                        logout.logoutUrl("/auth/logout")
+                                .deleteCookies("JSESSIONID")
+                                .invalidateHttpSession(true)
+                                .clearAuthentication(true)
+                                .logoutSuccessHandler((request, response, authentication) -> {
+                                    response.setStatus(HttpServletResponse.SC_OK);
+                                })
+                                .permitAll())
+                .build();
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider(customUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+    @Bean
+    public HttpSessionSecurityContextRepository securityContextRepository() {
+        return new HttpSessionSecurityContextRepository();
+    }
+}
